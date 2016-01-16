5cae34f4e678d0f58f310ed16e5aeecfe9d3f6c3
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2016-01-16 02:05:20 +0200

    Service layer + Tests

---
+++ BCT.Services/TokenizationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCT.Services
+{
+    public class TokenizationService
+    {
+        public static bool IsLuhnValid(string cardNumber)
+        {
+            int checksum = 0;
+            int doubledDigit = 0;
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                if (((cardNumber.Length - i) & 1) == 1)
+                {
+                    checksum += cardNumber[i] - '0';
+                }
+                else
+                {
+                    doubledDigit = 2 * (cardNumber[i] - '0');
+                    if (doubledDigit >= 10)
+                    {
+                        checksum += 1 + doubledDigit % 10;
+                    }
+                    else
+                    {
+                        checksum += doubledDigit;
+                    }
+                }
+            }
+
+            return checksum % 10 == 0;
+        }
+    }
+}
---
+++ BCT.ServicesTests/TokenizationServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BCT.Services;
+
+namespace BCT.ServicesTests
+{
+    [TestClass]
+    public class TokenizationServiceTest
+    {
+        [TestMethod]
+        public void Test1_IsLuhnValid()
+        {
+            string cardNumber = "79927398713";
+            Assert.AreEqual(true, TokenizationService.IsLuhnValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void Test2_IsLuhnValid()
+        {
+            string cardNumber = "4563960122019991";
+            Assert.AreEqual(true, TokenizationService.IsLuhnValid(cardNumber));
+        }
+    }
+}
